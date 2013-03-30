4f67b2f287fb39a7fe6a0f1b9b148e8c2af8d75b
Ankso <administrador.ankso@reinodelaoscuridad.com>
2013-03-30 05:50:08 +0100

    Eliminada la carpeta foro de la subweb de minecraft, no hace falta.'

--- minecraft/foro/plugins/QnA/locale/es-ES.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-
-// Discussion / Question
-$Definition['Activity.QuestionAnswer.FullHeadline'] = '%1$s respondió %4$s %8$s.';
-$Definition['Activity.AnswerAccepted.FullHeadline'] = '%1$s aceptó %4$s %8$s.';
-$Definition['Q&A Accepted'] = 'Respondida ✓';
