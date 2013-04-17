ea15522217820229e0a192d3760c3590b3cc56f9
Ankso <administrador.ankso@reinodelaoscuridad.com>
2013-04-18 01:23:07 +0200

    Rutas relativas a rutas completas.

--- common/Common.php
+++ common/Common.php
@@ -165,12 +165,12 @@ function PrintBottomBar()
 {
     echo '<div class="bottomBarContainer">
 		<div class="bottomBarLeft">&copy; 2013 Steel Gamers Community.<br>Todos los derechos reservados.</div>
-		<div class="bottomBarCenter"><a target="_blank" href="http://www.w3.org/html/wg/drafts/html/master/"><img src="images/html5_logo_32.png"></a></div>
+		<div class="bottomBarCenter"><a target="_blank" href="http://www.w3.org/html/wg/drafts/html/master/"><img src="/images/html5_logo_32.png"></a></div>
 		<div class="bottomBarRight">
 			<div class="bottomBarRightText">Steel Gamers recomienda: </div>
 			<div class="bottomBarRightIcons">
-				<a target="_blank" href="http://www.mozilla.org"><img style="margin-right:7px; width:31px;" src="images/firefox_32.png"></a>
-				<a target="_blank" href="http://google.com/chrome"><img src="images/chrome_32.png"></a>
+				<a target="_blank" href="http://www.mozilla.org"><img style="margin-right:7px; width:31px;" src="/images/firefox_32.png"></a>
+				<a target="_blank" href="http://google.com/chrome"><img src="/images/chrome_32.png"></a>
 			</div>
 		</div>
     </div>';
