106de15ba18a4f1a843a5048a88ab6327c500541
Ankso <misterankso@gmail.com>
2014-02-03 21:05:09 +0100

    Reparado un error descubierto en el ultimo commit, pero que parecia venir de atras.

--- design/right.php
+++ design/right.php
@@ -136,9 +136,13 @@
 		</div>
 		<?php
 		} if ($_Layout->GetLayoutOption(LAYOUT_SHOW_TWITTER)) { ?>
-		<div style="margin-top:10px;"><a class="twitter-timeline" href="https://twitter.com/SteelGamersSGC" <?php  echo $isDark ? 'data-theme="dark"' : '' ?> data-widget-id="326778925160202241">Tweets por @SteelGamersSGC</a>
-		<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-    <div class="g-page" data-href="https://plus.google.com/104321172907932626147/104321172907932626147" data-theme="dark" data-width="250"></div>
+		<div style="margin-top:10px;">
+      		<a class="twitter-timeline" href="https://twitter.com/SteelGamersSGC" <?php  echo $isDark ? 'data-theme="dark"' : '' ?> data-widget-id="326778925160202241">Tweets por @SteelGamersSGC</a>
+    	</div>
+    	<div style="margin-top:10px;">
+			<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+    		<div class="g-page" data-href="https://plus.google.com/104321172907932626147/104321172907932626147" <?php  echo $isDark ? 'data-theme="dark"' : '' ?> data-width="250"></div>
+		</div>
 		<?php } ?>
 	</div>
 </div>
